062e2639e9d95be78f83e58464648cab6ea56a0f
Anujmourya000 <Anujmourya000@gmail.com>
2021-05-08 16:33:24 +0530

    Added: part time employee is added

--- EmpWageComputationPart4.java
+++ EmpWageComputationPart4.java
@@ -13,25 +13,26 @@ public class EmpWageComputationPart4 {
 	private static final int IS_PRESENT = 1;
 	private int WAGE_PER_HOUR = 20;
 	private int FULL_DAY_HOUR = 8;
-	// randomly taking input
+	private int HALF_DAY_HOUR = 4;
+
 	public static double empCheck = (int) Math.floor(Math.random() * 10) % 2;
 
-	/**
-	 * @description Constructor of EmployeeWagesComputation class
-	 * @param None
-	 * 
-	 */
-	public EmpWageComputationPart4(int WAGE_PER_HOUR, int FULL_DAY_HOUR) {
+	// Constructor
+	public EmpWageComputationPart4(int WAGE_PER_HOUR, int FULL_DAY_HOUR, int HALF_DAY_HOUR) {
 		this.WAGE_PER_HOUR = WAGE_PER_HOUR;
 		this.FULL_DAY_HOUR = FULL_DAY_HOUR;
+		this.HALF_DAY_HOUR = HALF_DAY_HOUR;
 	}
 
 	public static void main(String[] args) {
 
 		if (empCheck == IS_PRESENT) {
-			EmpWageComputationPart4 empWage = new EmpWageComputationPart4(20, 8);
-			int dailyWages = (empWage.WAGE_PER_HOUR * empWage.FULL_DAY_HOUR);
-			System.out.println("Employee daily wages is: " + dailyWages);
+			EmpWageComputationPart4 empWage1 = new EmpWageComputationPart4(20, 8, 0);
+			EmpWageComputationPart4 empWage2 = new EmpWageComputationPart4(20, 0, 4);
+			int dailyWagesofFulltime = (empWage1.WAGE_PER_HOUR * empWage1.FULL_DAY_HOUR);
+			int dailyWagesofParttime = (empWage2.WAGE_PER_HOUR * empWage2.HALF_DAY_HOUR);
+			System.out.println("Full time Employee daily wages is: " + dailyWagesofFulltime);
+			System.out.println("Part time Employee daily wages is: " + dailyWagesofParttime);
 		} else {
 			System.out.println("Employee is absent");
 		}
