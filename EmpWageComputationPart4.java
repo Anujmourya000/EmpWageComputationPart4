ac67e038cce0174efeaf1b02562d0be6ad4bed3b
Anujmourya000 <Anujmourya000@gmail.com>
2021-05-08 16:18:34 +0530

    Added: Employee attendence check is added:

--- EmpWageComputationPart4.java
+++ EmpWageComputationPart4.java
@@ -1,9 +1,21 @@
 
+/**
+ * @description Class for defining the Employee Wages Computation
+ * @param None
+ * 
+ */
 public class EmpWageComputationPart4 {
+	private static final int IS_PRESENT = 1;
+
+	// Taking values randomly
+	public static double empCheck = Math.floor(Math.random() * 10) % 2;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		if (empCheck == IS_PRESENT) {
+			System.out.println("Employee is present");
+		}
+		else
+			System.out.println("Employee is absent");
 	}
-
 }
